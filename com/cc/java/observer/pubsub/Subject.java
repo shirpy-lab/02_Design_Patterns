b098db7c3d30b9ef74a5452fa0ea01dc8b063580
shirpy-lab <shirleybkel@gmail.com>
2025-04-10 10:45:11 +0200

    Update Subject.java

--- com/cc/java/observer/pubsub/Subject.java
+++ com/cc/java/observer/pubsub/Subject.java
@@ -8,7 +8,53 @@ import com.cc.java.tools.Helper;
 
 public class Subject implements Observable{
 
+    private String state;
+    private List<Observer>observers;
+
+    public Subject() {
+       observers = new ArrayList<>();
+    }
+
+    /* Abonnenten-Management */
    
+    @Override
+    public void attach(Observer o) {
+        observers.add(o);  // Observer tragen sich in Abo-Liste ein
+    }
+
+    @Override
+    public void detatch(Observer o) {
+        observers.remove(o); // Observer lassen sich aus Abo-Liste löschen
+    }
+
+    /* Status */
+
+    // Subjekt ändert seinen Status (Post/Video ...)
+    public void setState(String state) {
+        this.state = state;
+        Helper.output("Subject's new state: " + state);
+        notifyObservers();
+    }
+
+    /* Benachrichtigung der Abonennten*/
+
+    @Override
+    public void notifyObservers() {
+        for (Observer o : observers) {
+            if (o.isPushed()) {
+                o.update(state); // Push
+            } else {
+                o.update();  // Pull
+            }
+        }
+    }
+
+      // Observer holen sich den neuen Status ...
+    public String getState() {
+        return state;
+    }
+
+  
 
 
 }
